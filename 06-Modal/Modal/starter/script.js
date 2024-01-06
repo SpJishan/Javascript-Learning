f506d8e81be460094485c137561a15336536f6ab
Shafinul Pasha <jishan523@gmail.com>
2024-01-06 11:59:54 +0600

    Modal Window ( Defining initial steps)
     Pin points the objectives , implications

--- 06-Modal/Modal/starter/script.js
+++ 06-Modal/Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//*******Objectives:
+
+// 1. We will click a button, (that is ''show-modal'' class , there are multiple show-modal so we should use "document.queryselctorAll")
+// 2. That will open a modal window that also generate a overlay background
+
+// 3. It will have a cross button to close. Also while clicking on overlay area it will close.
+
+// //*****Implications: */
+
+// 1. To click a button some steps should be completed:
+// -We have to store the functionality to "modal" class in a variable,(that is the window going to view).
+// -Same as for "overlay,show-modal & close-modal " classes
+
+// 2. We will call the variables to open and close the modals
+
+
+
+
+
+
+
