678ad807a4358d851df562ed8f805b51e741edf3
Shafinul Pasha <jishan523@gmail.com>
2024-01-07 12:26:19 +0600

    Dice-Rolling-game-Initial SetUp Created HTML, CSS and script file.
    Setting the player score value to initial value

---
+++ 07-Dice-Rolling-Game/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+//Set the two player scores 0
+
+const score0El=document.querySelector('#score--0');
+const score1El=document.getElementById('score--1'); //DOM selection for ID(New method)
+
+score0El.textContent=0;
+score1El.textContent=0;
+
+
