39673c835e96ba3433950c7f001160a84f1624c3
Shafinul Pasha <jishan523@gmail.com>
2024-01-08 14:12:55 +0600

    Dice Rolling Game-Switching the Active Player
    1. To change the player I use player element variable
    
    2.  Player scores are stored in array score
    
    3. I have to manipulate the current score elements and will do it by "activePlayer" variable and so we set it's value 0
    
    4. Selecting the current score elements dynamically , we start from player 1 so the activePlayer is set to 0
    
    5.Logic- Switching the active players using ternary operator(If the activePlayer is 0 (that is activePlayer===0) then the value should be 1 otherwise 0)
    
    6. Switching players using toggle classlist.

--- 07-Dice-Rolling-Game/starter/script.js
+++ 07-Dice-Rolling-Game/starter/script.js
@@ -16,7 +16,9 @@ const btnNew=document.querySelector('.btn--new');
 const btnRoll=document.querySelector('.btn--roll');
 const btnHold=document.querySelector('.btn--hold');
 const diceEl=document.querySelector('.dice');
-
+            //Player Element
+const player0El=document.querySelector('.player--0');
+const player1El=document.querySelector('.player--1');
 
 score0El.textContent=0;
 score1El.textContent=0;
@@ -26,25 +28,31 @@ score1El.textContent=0;
         //Make the dice hidden
 diceEl.classList.add('.hidden');
 
-        //Rolling dice functionality
+        // Player scores are stored
+const score=[0,0] 
 let currentScore=0;
+let activePlayer=0; //to manipulate the current score elements and will do it by "activePlayer" variable and so we set it's value 0
+
+        //Rolling dice functionality- 
 btnRoll.addEventListener('click', function(){
-    //Generting a random dice roll
-    const dice= Math.trunc(Math.random()*6)+1;
+    const dice= Math.trunc(Math.random()*6)+1;  //Generting a random number for dice roll
     console.log(dice);
 
-    //display dice
+        //display dice
     diceEl.classList.remove('.hidden');
-    diceEl.src= `dice-${dice}.png`;
+    diceEl.src= `dice-${dice}.png`;  
 
     //Check for 1 then switch players
 
     if(dice!==1){
         currentScore += dice; //Add dice to current score
-        current0El.textContent=currentScore; //change later
+        document.getElementById(`current--${activePlayer}`).textContent=currentScore;  //Selecting the current score elements dynamically , we start from player 1 so the activePlayer is set to 0
+        // current0El.textContent=currentScore; //change later
         
     } else{
-        //switch player
+        activePlayer= activePlayer===0 ? 1: 0; //If the activePlayer is 0 (that is activePlayer===0) then the value should be 1 otherwise 0
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 })
 
