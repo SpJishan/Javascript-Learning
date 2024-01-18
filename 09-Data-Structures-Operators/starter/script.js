6f2d8c01ff28a3b0674e9bc9c2f430f7a3588066
Shafinul Pasha <jishan523@gmail.com>
2024-01-18 19:02:18 +0600

    The Basics-Rest Pattern and Parameters
    Rest operators working on Objects
    
    Rest on functions
    
    Rest on function perameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -47,6 +47,11 @@ const restaurant = {
     console.log(`You added ${ing1}, ${ing2}, ${ing3} to your pasta.`);
   },
 
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+
 
   openingHours: {
     thu: {
@@ -183,4 +188,55 @@ const restaurant = {
 // console.log(restaurantNew.name); // Displaying new name
 
 
+//////////////////////////////////////////////////////////////////////////////////////////
+//                         Rest Pattern and Parameters 
+//////////////////////////////////////////////////////////////////////////////////////////
 
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//                         The Basics
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//Spread operator unpack an array
+//Rest operator packed an array
+
+// Spread works with value , used on right side of =
+const arr= [1, 2, 3, ...[4,5]] ;
+console.log(arr);
+
+// Rest works with variable , used on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+// The rest operator should be the last element, so there should be one rest operators
+
+const [pizza , ...otherFood]= [...restaurant.starterMenu, ...restaurant.mainMenu]
+console.log(pizza);
+console.log(...otherFood);
+
+//Rest operators working on Objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//Rest on functions
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++)
+   sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+//Rest operators on function perameters
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
