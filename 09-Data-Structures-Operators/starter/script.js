98edaeeb5ed8154f3a747d9826fab4de2f398856
Shafinul Pasha <jishan523@gmail.com>
2024-01-19 17:26:12 +0600

    107. OR, && Operators ShortCircuiting 1. OR operator will return first truthy value
    2. AND operator will return first falsie value
    
    3. using if/tarnary logic to check if a method exist

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -203,40 +203,77 @@ const restaurant = {
 //Rest operator packed an array
 
 // Spread works with value , used on right side of =
-const arr= [1, 2, 3, ...[4,5]] ;
-console.log(arr);
+// const arr= [1, 2, 3, ...[4,5]] ;
+// console.log(arr);
 
-// Rest works with variable , used on left side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+// // Rest works with variable , used on left side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
 
-// The rest operator should be the last element, so there should be one rest operators
+// // The rest operator should be the last element, so there should be one rest operators
 
-const [pizza , ...otherFood]= [...restaurant.starterMenu, ...restaurant.mainMenu]
-console.log(pizza);
-console.log(...otherFood);
+// const [pizza , ...otherFood]= [...restaurant.starterMenu, ...restaurant.mainMenu]
+// console.log(pizza);
+// console.log(...otherFood);
 
-//Rest operators working on Objects
+// //Rest operators working on Objects
 
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
 
-//Rest on functions
+// //Rest on functions
 
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++)
-   sum += numbers[i];
-  console.log(sum);
-};
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++)
+//    sum += numbers[i];
+//   console.log(sum);
+// };
 
-add(2, 3);
-add(5, 3, 7, 2);
-add(8, 2, 5, 3, 2, 1, 4);
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1, 4);
 
-const x = [23, 5, 7];
-add(...x);
+// const x = [23, 5, 7];
+// add(...x);
 
-//Rest operators on function perameters
-restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
-restaurant.orderPizza('mushrooms');
+// //Rest operators on function perameters
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+// restaurant.orderPizza('mushrooms');
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//                         OR, && Operators ShortCircuiting 
+//////////////////////////////////////////////////////////////////////////////////////////
+
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, short-circuiting
+//OR operator will return first truthy value
+console.log(3 || 'Jishan');
+console.log('' || 'Jishan');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+//AND operator will return first falsie value
+console.log(0 && 'Jishan');
+console.log(7 && 'Jishan');
+
+console.log('Hello' && 23 && null && 'Jishan');
+
+// Practical example
+//if logic to check if a method exist
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+//Tarnary logic to check if a method exist
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
