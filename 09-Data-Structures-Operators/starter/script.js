4e444b75adf8f86f8acce249a3aac041f1279dd7
Shafinul Pasha <jishan523@gmail.com>
2024-01-20 21:08:01 +0600

    111 Optional Chaining  w
     Usually it inquiries if a method exists than apply the other codes

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -89,20 +89,6 @@ const openingHours = { //1st way: we can declare an object outside of another ob
   },
 };
 
-const openingHours= { 
-  thu: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  sat: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
 
 const restaurant = {
   name: 'Classico Italiano',
@@ -492,22 +478,56 @@ const game = {
 //                        110 Looping Arrays The for of Loop
 //////////////////////////////////////////////////////////////////////////////////////////
 
-const menu = [...restaurant.starterMenu,...restaurant.mainMenu] //Joining two array into one variable
+// const menu = [...restaurant.starterMenu,...restaurant.mainMenu] //Joining two array into one variable
 
-console.log(menu);
+// console.log(menu);
 
-for (const item of menu){ //for of loop
-  console.log(item);
+// for (const item of menu){ //for of loop
+//   console.log(item);
+// }
+
+// console.log(...menu.entries());
+
+// //Old Ways to use for of loop
+// for (const item of menu.entries()){     
+//   console.log(`${item[0]+1}: ${item[1]}`);
+// }
+
+// //New Way to use for of loop
+// for (const [i, el] of menu.entries()){     // we know the resut is stored in two array so we destructuring those array
+//   console.log(`${i+1}: ${el}`);
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//                        111 Optional Chaining
+//////////////////////////////////////////////////////////////////////////////////////////
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining c
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
 }
 
-console.log(...menu.entries());
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
-//Old Ways to use for of loop
-for (const item of menu.entries()){     
-  console.log(`${item[0]+1}: ${item[1]}`);
-}
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
 
-//New Way to use for of loop
-for (const [i, el] of menu.entries()){     // we know the resut is stored in two array so we destructuring those array
-  console.log(`${i+1}: ${el}`);
-}
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty');
