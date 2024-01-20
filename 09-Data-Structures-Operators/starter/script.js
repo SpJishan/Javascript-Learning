46ba58f4415b56073fb464e47f634bd09b1c709f
Shafinul Pasha <jishan523@gmail.com>
2024-01-20 19:33:28 +0600

    110 Looping Arrays The for of Loop
    417. Joining two array into one variable
    
    421. For of loop
    
    427. Old Ways to use for of loop
    
    432.New Way to use for of loop
    433. we know the resut is stored in two array so we destructuring those array

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -356,56 +356,80 @@ const game = {
   },
 };
 
+// //////////////////////////////////////////////////////////////////////////////////////////
+// //1. Create one player array for each team (variables 'players1' and 'players2')
+// //////////////////////////////////////////////////////////////////////////////////////////
+
+// const [players1, players2]= game.players; //Setting array variables from object's array
+// console.log(players1, players2)
+
+// //////////////////////////////////////////////////////////////////////////////////////////
+// //2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// //////////////////////////////////////////////////////////////////////////////////////////
+
+// const [gk, ...fieldPlayers]= players1; //Using rest operators, assigning 1st member then remainings player
+// console.log(gk, fieldPlayers);
+
+// //////////////////////////////////////////////////////////////////////////////////////////
+// //3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// //////////////////////////////////////////////////////////////////////////////////////////
+
+// const allPlayers =[...players1,...players2];  //Using spread operator 2 array is joined to one array
+// console.log(allPlayers);
+
+// //////////////////////////////////////////////////////////////////////////////////////////
+// //4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// //////////////////////////////////////////////////////////////////////////////////////////
+
+// const players1Final =[...players1,'Thiago','Continho','Perisic']; //Using spread operator and playername to update all payers to a final variable
+// console.log(players1Final);
+
+// //////////////////////////////////////////////////////////////////////////////////////////
+// //5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// //////////////////////////////////////////////////////////////////////////////////////////
+
+// const {odds: { team1, x:draw , team2}} =game; //Setting the variable name on a nested destructuring , here we rename x to draw, It's a compex concept
+
+// console.log(team1,draw, team2);
+
+// //////////////////////////////////////////////////////////////////////////////////////////
+// //6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// //////////////////////////////////////////////////////////////////////////////////////////
+
+// const printGoals= function(...players){ // Recieve an array of perameters
+//   console.log(players); //Dispalayng the array
+//   console.log(`${players.length} goles are scored`); //Number of array are goaled
+// }
+
+// printGoals(...game.scored);
+
+// //////////////////////////////////////////////////////////////////////////////////////////
+// //7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+// //////////////////////////////////////////////////////////////////////////////////////////
+
+// team1 < team2 && console.log('Team 1 is more likely to win'); //passed the condition and print
+// team1 > team2 && console.log('Team 2 is more likely to win');
+
 //////////////////////////////////////////////////////////////////////////////////////////
-//1. Create one player array for each team (variables 'players1' and 'players2')
+//                        110 Looping Arrays The for of Loop
 //////////////////////////////////////////////////////////////////////////////////////////
 
-const [players1, players2]= game.players; //Setting array variables from object's array
-console.log(players1, players2)
+const menu = [...restaurant.starterMenu,...restaurant.mainMenu] //Joining two array into one variable
 
-//////////////////////////////////////////////////////////////////////////////////////////
-//2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-//////////////////////////////////////////////////////////////////////////////////////////
+console.log(menu);
 
-const [gk, ...fieldPlayers]= players1; //Using rest operators, assigning 1st member then remainings player
-console.log(gk, fieldPlayers);
-
-//////////////////////////////////////////////////////////////////////////////////////////
-//3. Create an array 'allPlayers' containing all players of both teams (22 players)
-//////////////////////////////////////////////////////////////////////////////////////////
-
-const allPlayers =[...players1,...players2];  //Using spread operator 2 array is joined to one array
-console.log(allPlayers);
-
-//////////////////////////////////////////////////////////////////////////////////////////
-//4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-//////////////////////////////////////////////////////////////////////////////////////////
-
-const players1Final =[...players1,'Thiago','Continho','Perisic']; //Using spread operator and playername to update all payers to a final variable
-console.log(players1Final);
-
-//////////////////////////////////////////////////////////////////////////////////////////
-//5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-//////////////////////////////////////////////////////////////////////////////////////////
-
-const {odds: { team1, x:draw , team2}} =game; //Setting the variable name on a nested destructuring , here we rename x to draw, It's a compex concept
-
-console.log(team1,draw, team2);
-
-//////////////////////////////////////////////////////////////////////////////////////////
-//6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-//////////////////////////////////////////////////////////////////////////////////////////
-
-const printGoals= function(...players){ // Recieve an array of perameters
-  console.log(players); //Dispalayng the array
-  console.log(`${players.length} goles are scored`); //Number of array are goaled
+for (const item of menu){ //for of loop
+  console.log(item);
 }
 
-printGoals(...game.scored);
+console.log(...menu.entries());
 
-//////////////////////////////////////////////////////////////////////////////////////////
-//7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
-//////////////////////////////////////////////////////////////////////////////////////////
+//Old Ways to use for of loop
+for (const item of menu.entries()){     
+  console.log(`${item[0]+1}: ${item[1]}`);
+}
 
-team1 < team2 && console.log('Team 1 is more likely to win'); //passed the condition and print
-team1 > team2 && console.log('Team 2 is more likely to win');
+//New Way to use for of loop
+for (const [i, el] of menu.entries()){     // we know the resut is stored in two array so we destructuring those array
+  console.log(`${i+1}: ${el}`);
+}
