7586b4657c2862d4f5984a87e9dd4fe942ad9e74
Shafinul Pasha <jishan523@gmail.com>
2024-01-20 11:52:53 +0600

    Coding Challenge: rest, spread, nested destructuring, spread oprator in function perameters, logical opertors 363. Setting array variables from object's array 370. Using rest operators, assigning 1st member then remainings player 377. Using spread operator 2 array is joined to one array
    384. Using spread operator and playername to update all payers to a final variable
    
    391. Setting the variable name on a nested destructuring , here we rename x to draw, It's a
    compex concept
    
    399. Recieve an array of perameters
    400. Dispalayng the array
    401. Number of array are goaled
    
    410. Passed the condition and print

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -283,11 +283,129 @@ const restaurant = {
 //////////////////////////////////////////////////////////////////////////////////////////
 
 
-// The Nullish Coalescing Operator
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10; //Null and undefined is falsie value so turthy value 10 is return
-console.log(guests);
+// // The Nullish Coalescing Operator
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10; //Null and undefined is falsie value so turthy value 10 is return
+// console.log(guests);
 
-// Nullish: null and undefined (NOT 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10; //0 or empty string as truthy value so 0 is returned
-console.log(guestCorrect)
+// // Nullish: null and undefined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10; //0 or empty string as truthy value so 0 is returned
+// console.log(guestCorrect)
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//                        Coding Challenge: Array 
+//////////////////////////////////////////////////////////////////////////////////////////
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//1. Create one player array for each team (variables 'players1' and 'players2')
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const [players1, players2]= game.players; //Setting array variables from object's array
+console.log(players1, players2)
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const [gk, ...fieldPlayers]= players1; //Using rest operators, assigning 1st member then remainings player
+console.log(gk, fieldPlayers);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//3. Create an array 'allPlayers' containing all players of both teams (22 players)
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const allPlayers =[...players1,...players2];  //Using spread operator 2 array is joined to one array
+console.log(allPlayers);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const players1Final =[...players1,'Thiago','Continho','Perisic']; //Using spread operator and playername to update all payers to a final variable
+console.log(players1Final);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const {odds: { team1, x:draw , team2}} =game; //Setting the variable name on a nested destructuring , here we rename x to draw, It's a compex concept
+
+console.log(team1,draw, team2);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const printGoals= function(...players){ // Recieve an array of perameters
+  console.log(players); //Dispalayng the array
+  console.log(`${players.length} goles are scored`); //Number of array are goaled
+}
+
+printGoals(...game.scored);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+//////////////////////////////////////////////////////////////////////////////////////////
+
+team1 < team2 && console.log('Team 1 is more likely to win'); //passed the condition and print
+team1 > team2 && console.log('Team 2 is more likely to win');
