b8947e7ec6bf6b4e26b1033531d9ec48e1769a08
Shafinul Pasha <jishan523@gmail.com>
2024-01-27 07:17:46 +0600

    806 Functions Accepting Callback Functions
    65. "/ /g"-The "g" after the regular expression is an option or flag that performs a global search, looking in the whole string and returning all matches.
    
    72 . Making the first word uppercase
    
    79. Higher Order function
    
    97. This is an example of hgher order function.

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -36,27 +36,67 @@
 //   804. Value vs Reference 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-const flight = 'LH123';
-const jishan = {
-    name: 'Shafinul Pasha Jishan',
-    passport: 7123,
+// const flight = 'LH123';
+// const jishan = {
+//     name: 'Shafinul Pasha Jishan',
+//     passport: 7123,
+// };
+
+// const checkIn = function (flightNum, passenger){
+//     passenger.name='MR. ' + passenger.name;
+//     console.log(passenger.passport);
+//     if(passenger.passport === 7123 ){
+//         console.log(`Checked In`);
+//     }else{
+//         console.log(`Wrong Passport`);
+//     }
+// };
+
+// checkIn(flight, jishan);
+// console.log(flight);
+// console.log(jishan);
+
+// jishan.passport = 8123;
+// checkIn(flight, jishan); //The passport value has changed , so the function is using jishan.passport as object reference but using or calling it's updated value than the reference value.So in JavaScript functions are called by its value not reference
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//   806 Functions Accepting Callback Functions 
+/////////////////////////////////////////////////////////////////////////////////////////
+// "/ /g"-The "g" after the regular expression is an option or flag that performs a global search, looking in the whole string and returning all matches.
+const lowerCase = function(str){
+    return str.replace(/ /g, ' ').toLowerCase(); 
 };
 
-const checkIn = function (flightNum, passenger){
-    passenger.name='MR. ' + passenger.name;
-    console.log(passenger.passport);
-    if(passenger.passport === 7123 ){
-        console.log(`Checked In`);
-    }else{
-        console.log(`Wrong Passport`);
-    }
+
+
+//Making the first word uppercase
+
+const upperCase = function(str) {
+    const [first, ...others] =str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
 };
 
-checkIn(flight, jishan);
-console.log(flight);
-console.log(jishan);
+//Higher Order function
 
-jishan.passport = 8123;
-checkIn(flight, jishan); //The passport value has changed , so the function is using jishan.passport as object reference but using or calling it's updated value than the reference value.So in JavaScript functions are called by its value not reference
+const transformer = function(str, fn){
+    console.log(`Original String: ${str}`);
+    console.log(`Transformed String: ${fn(str)}`);
+
+    console.log(`Transformed Function: ${fn.name}`);
+}
+
+transformer(`toDay is a beautiful day`, lowerCase);
+transformer(`toDay is a beautiful day`, upperCase);
+
+//Simple eventlistener 
+
+const HI5 = function(){
+    console.log('🖐');
+};
+
+document.body.addEventListener('click' , HI5); //This is an example of hgher order function.
+
+
+
 
 
