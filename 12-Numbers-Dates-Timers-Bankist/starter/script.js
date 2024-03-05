47f538bf59de0381e875901310a250a4b251ea0d
Shafinul Pasha <jishan523@gmail.com>
2024-03-05 17:50:21 +0600

    Math and Rounding
    1. For Square & Cubic roots
    
    2. Finding max and min numbers- can do type conversion
    
    3. Calculating Circle
    
    4. Generating Random Number
    
    5. Rounding integers
    
    6. Rounding decimals

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -259,39 +259,86 @@ btnSort.addEventListener('click', function (e) {
 // Converting and Checking Numbers
 /////////////////////////////////////////////////
 
-console.log(23 === 23.0);
+// console.log(23 === 23.0);
 
-// 1.1 Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.3333333
-// 1.2 Binary base 2 - 0 1
-console.log(0.1 + 0.2);
-console.log(0.1 + 0.2 === 0.3);
+// // 1.1 Base 10 - 0 to 9. 1/10 = 0.1. 3/10 = 3.3333333
+// // 1.2 Binary base 2 - 0 1
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 === 0.3);
 
-// 2. Number Conversion
-console.log(Number('23'));
-console.log(+'23');
+// // 2. Number Conversion
+// console.log(Number('23'));
+// console.log(+'23');
 
-// 3. Parsing
-console.log(Number.parseInt('30px', 10));
-console.log(Number.parseInt('e23', 10));
+// // 3. Parsing
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('e23', 10));
 
-console.log(Number.parseInt('  2.5rem  '));
-console.log(Number.parseFloat('  2.5rem  '));
+// console.log(Number.parseInt('  2.5rem  '));
+// console.log(Number.parseFloat('  2.5rem  '));
 
-// console.log(parseFloat('  2.5rem  '));
+// // console.log(parseFloat('  2.5rem  '));
 
-// 4. Check if value is NaN
-console.log(Number.isNaN(20));
-console.log(Number.isNaN('20'));
-console.log(Number.isNaN(+'20X'));
-console.log(Number.isNaN(23 / 0));
+// // 4. Check if value is NaN
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0));
 
-// 5. Checking if value is number
-console.log(Number.isFinite(20));
-console.log(Number.isFinite('20'));
-console.log(Number.isFinite(+'20X'));
-console.log(Number.isFinite(23 / 0));
+// // 5. Checking if value is number
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20X'));
+// console.log(Number.isFinite(23 / 0));
 
-console.log(Number.isInteger(23));
-console.log(Number.isInteger(23.0));
-console.log(Number.isInteger(23 / 0));
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23 / 0));
 
+/////////////////////////////////////////////////
+// Math and Rounding
+/////////////////////////////////////////////////
+
+// 1. For Square & Cubic roots
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+// 2. Finding max and min numbers- can do type conversion
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2)); 
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+// 3. Calculating Circle
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// 4. Generating Random Number
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0...1 -> 0...(max - min) -> min...max
+// console.log(randomInt(10, 20));
+
+// 5. Rounding integers
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
+
+console.log(Math.trunc(23.3));
+
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+// 6. Rounding decimals
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
