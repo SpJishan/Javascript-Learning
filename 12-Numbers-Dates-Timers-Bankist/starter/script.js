44df4f59fe22a6e48ceebee3f1aff486f2313726
Shafinul Pasha <jishan523@gmail.com>
2024-03-08 19:52:33 +0600

    Implmenting a countdown timer
    1. startLogOutTimer function
    
    2. In each call, print the remaining time to UI
    
    3. When 0 seconds, stop timer and log out user
    
    4. Decrease 1s
    
    5. Set time to 2 minutes
    
    6. Call the timer every second
    
    7. Applied on login- clearing the muliple login timer.
    
    8. Reseting the timer on transfer and loan

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -199,9 +199,38 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+
+    // In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 seconds, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = "Log in to get started";
+      containerApp.style.opacity = 0;
+    }
+
+    // Decrease 1s
+    time--;
+  };
+
+  // Set time to 2 minutes
+  let time = 120;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+
+  return timer;
+};
+
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 ///////////////////////////////////////
 // Adding Dates to Bankist App
@@ -209,9 +238,9 @@ let currentAccount;
 
 // 1. Fake Loggind in to manipulates diff date changes
 
-currentAccount = account1
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// currentAccount = account1
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 
 
@@ -255,6 +284,9 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+    
     // Update UI
     updateUI(currentAccount);
   }
