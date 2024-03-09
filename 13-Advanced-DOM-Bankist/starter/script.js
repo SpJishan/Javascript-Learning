d221d04318c14c40cf93ac20067318981f852f4f
Shafinul Pasha <jishan523@gmail.com>
2024-03-09 09:21:07 +0600

    Styles, Attributes and Classes
    **Style->
    select_element>style>default_attribute
    
    For undefined style we use
    getComputedStyle- sometimes we did not define a style attribute , but automatically it applied on browser , but later on if we need to change it , we can use it
    
    setProperty- Custom property for CSS
    
    **Attribute->
    We can not directly select an attribute.In this case we will need to select the element first. Remember we can only use default attributes.
    
    getAttribute->To use a custom attributes we need to use getAttribute
    setAttribute(_ , _)->
    
    data Attribute- If we wrote in html data-version-number then in script we select element then dataset.versionNumber ( name needs to be camel case), remember the naming convention between html and js
    
    **Classes-
    element.classList.add
    element.classList.remove
    element.classList.toggle
    element.classList.contains

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -37,39 +37,86 @@ document.addEventListener('keydown', function (e) {
 // Selecting, Creating, and Deleting Elements
 ///////////////////////////////////////////////////////////////////////////////////////
 
-// Selecting elements
-console.log(document.documentElement); // 1. to select overal html
-console.log(document.head);
-console.log(document.body);
+// // Selecting elements
+// console.log(document.documentElement); // 1. to select overal html
+// console.log(document.head);
+// console.log(document.body);
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
-// Creating and inserting elements
-const message = document.createElement('div');
-message.classList.add('cookie-message'); // 2. Added a classlist from css
-// message.textContent = 'We use cookied for improved functionality and analytics.';
-message.innerHTML =
-  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// // Creating and inserting elements
+// const message = document.createElement('div');
+// message.classList.add('cookie-message'); // 2. Added a classlist from css
+// // message.textContent = 'We use cookied for improved functionality and analytics.';
+// message.innerHTML =
+//   'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-header.prepend(message);
-// header.append(message);
-// header.append(message.cloneNode(true));
+// header.prepend(message);
+// // header.append(message);
+// // header.append(message.cloneNode(true));
 
-// header.before(message);
-// header.after(message);
+// // header.before(message);
+// // header.after(message);
 
-// Delete elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-    // message.parentElement.removeChild(message);
-  });
+// // Delete elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//     // message.parentElement.removeChild(message);
+//   });
+
+///////////////////////////////////////////////////////////////////////////////////////
+// Styles, Attributes and Classes
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height); //For undefined style we use
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');//  Custom property for CSS
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer')); // To use a custom attributes we need to use getAttribute. designer-not a default attributes(src, alt)
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber); // remember the naming convention between html and js
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
