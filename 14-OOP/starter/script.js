0010c2e3bf890918a45b0ab035b4fea7399d7371
Shafinul Pasha <jishan523@gmail.com>
2024-03-20 11:33:17 +0600

    Prototypal Inheritance on Built-In Objects
    Different prototypical chain logged in the codebase , for example the h1 has 7 levels of protypical chain .

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -59,3 +59,28 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species')); // it will be false because it has access to jonas by prototype but not in the jonas object
+
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
