5bd72401e006875cb0ab61f35b920c4629b1a44c
Shafinul Pasha <jishan523@gmail.com>
2024-03-25 11:29:51 +0600

    Displaying a Map Using Leaflet Library
    1. Adding a Leaflet  CDN to the html heads. Notice the defer attribute
    
    2. Copy the leaflet code from overview to show a marker in map
    
    3. Replacing  co-ordinates array with coords, beside we can also modify the zoom property

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,14 +11,33 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(function(position){  // Gettinng the latitude and longitude
+if(navigator.geolocation){
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // Gettinng the latitude and longitude
     // const latitude = position.coords.latitude;
     // const longitude = position.coords.longitude;
-    const {latitude} = position.coords;    // New structuring
-    const {longitude} = position.coords;
+    const { latitude } = position.coords; // New structuring
+    const { longitude } = position.coords;
 
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);  // Converted it to google links
-    
-}, function(){
+    const coords = [latitude, longitude]
+
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`); // Converted it to google links
+    const map = L.map('map').setView(coords, 10); //Replacing  co-ordinates array with coords
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
     alert(`Could not get your position`);
-});
+  }
+);
+
+}
