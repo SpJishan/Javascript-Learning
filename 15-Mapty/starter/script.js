4aaeb545eee079c274568af9756933eb2cdc4f94
Shafinul Pasha <jishan523@gmail.com>
2024-03-25 10:59:59 +0600

    Using the Geolocation API
    Gettinng the latitude and longitude
    
    New structuring
    
    Converted it to google links

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,15 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(function(position){  // Gettinng the latitude and longitude
+    // const latitude = position.coords.latitude;
+    // const longitude = position.coords.longitude;
+    const {latitude} = position.coords;    // New structuring
+    const {longitude} = position.coords;
+
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);  // Converted it to google links
+    
+}, function(){
+    alert(`Could not get your position`);
+});
