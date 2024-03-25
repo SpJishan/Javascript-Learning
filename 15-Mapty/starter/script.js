8a47a54367e81bfd32f8e00b0c44986e295df458
Shafinul Pasha <jishan523@gmail.com>
2024-03-25 14:47:38 +0600

    Rendering Workout Input Form
    To display form, removed the classlist hidden
    
    Focus input to the distance field
    
    Rearrange code to a form event listner to  Display Marker
    
    Setting global variable for map and mapEvent to acces the rearranged code
    
    Preventing default for form input
    
    Clearing the input field after typing and hidding the form
    
    Selcting the type field it also change the cadencce to elevation

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -23,33 +25,72 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`); // Converted it to google links
-      const map = L.map('map').setView(coords, 10); //Replacing  co-ordinates array with coords
+      map = L.map('map').setView(coords, 10); //Replacing  co-ordinates array with coords
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {  //.on (map.on) is a leaflet property not javaScript
+      map.on('click', function (mapE) {
+        //.on (map.on) is a leaflet property not javaScript
         // Adding Click event on the map
+        mapEvent = mapE;
         console.log(mapEvent);
-        const { lat, lng} = mapEvent.latlng;
+        form.classList.remove('hidden'); //1. To display form, removed the classlist hidden
+        inputDistance.focus(); //2. Focus input to the distance field
 
-        L.marker([lat, lng])  // setting lat lng
-          .addTo(map)
-          .bindPopup(L.popup({  // From leaflet documentaion in marker added different property to the popup
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          }))
-          .setPopupContent('Workout')
-          .openPopup();
+        // const { lat, lng} = mapEvent.latlng;
+
+        // L.marker([lat, lng])  // setting lat lng
+        //   .addTo(map)
+        //   .bindPopup(L.popup({  // From leaflet documentaion in marker added different property to the popup
+        //     maxWidth: 250,
+        //     minWidth: 100,
+        //     autoClose: false,
+        //     closeOnClick: false,
+        //     className: 'running-popup',
+        //   }))
+        //   .setPopupContent('Workout')
+        //   .openPopup();
       });
     },
     function () {
       alert(`Could not get your position`);
     }
   );
+
+  form.addEventListener('submit', function (e) {
+    // Rearrange code to a form event listner to  Display Marker
+    e.preventDefault();
+
+    // Clearing the input field after typing
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+
+    const { lat, lng } = mapEvent.latlng;
+    L.marker([lat, lng]) // setting lat lng
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          // From leaflet documentaion in marker added different property to the popup
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  });
+
+//   Selcting the type field it also change the cadencce to elevation
+  inputType.addEventListener('change', function(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  })
 }
