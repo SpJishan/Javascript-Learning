365296e4f40e58acc78b963151ce554e6c8e380a
Shafinul Pasha <jishan523@gmail.com>
2024-03-25 12:05:34 +0600

    Displaying a Map Marker
    .on (map.on) is a leaflet property not javaScript
    
    Adding Click event on the map
    
    Setting lat lng
    
    From leaflet documentaion in marker added different property to the popup and styled it
    
    Adding setPopupContent('Workout')

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,33 +11,45 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if(navigator.geolocation){
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    // Gettinng the latitude and longitude
-    // const latitude = position.coords.latitude;
-    // const longitude = position.coords.longitude;
-    const { latitude } = position.coords; // New structuring
-    const { longitude } = position.coords;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // Gettinng the latitude and longitude
+      // const latitude = position.coords.latitude;
+      // const longitude = position.coords.longitude;
+      const { latitude } = position.coords; // New structuring
+      const { longitude } = position.coords;
 
-    const coords = [latitude, longitude]
+      const coords = [latitude, longitude];
 
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`); // Converted it to google links
-    const map = L.map('map').setView(coords, 10); //Replacing  co-ordinates array with coords
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`); // Converted it to google links
+      const map = L.map('map').setView(coords, 10); //Replacing  co-ordinates array with coords
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
-  },
-  function () {
-    alert(`Could not get your position`);
-  }
-);
+      map.on('click', function (mapEvent) {  //.on (map.on) is a leaflet property not javaScript
+        // Adding Click event on the map
+        console.log(mapEvent);
+        const { lat, lng} = mapEvent.latlng;
 
+        L.marker([lat, lng])  // setting lat lng
+          .addTo(map)
+          .bindPopup(L.popup({  // From leaflet documentaion in marker added different property to the popup
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          }))
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert(`Could not get your position`);
+    }
+  );
 }
