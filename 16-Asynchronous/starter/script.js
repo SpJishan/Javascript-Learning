02590ae48a602abbd0574258cbff124c8a82b4da
Shafinul Pasha <jishan523@gmail.com>
2024-03-29 16:30:44 +0600

    First AJAX Call: XMLHttpRequest
    1. Doing the old school way  new XMLHttpRequest()- for how AJAX calls used to be handled with events and callback functions.On public apis in github notice that CORS stands for Cross Origin Resource Sharing. On API call from github it need to show yes.
    
    2. request.open() ,will get two parameters, GET method from API and
    
    3. request.send() here, we basically send off the request. And so that request then fetches the data in the background.
    
    4. An event listener to 'load' event. When the sedn request is done it will emit the load event.
      ->4.1 JSON.parse will retrive data in array format , so we need to destructure that to an object. So the const data will be const [data]
    
    5. Making the card component.
      -> 5.1 Copied the html part and stored it to html
      -> 5.2 Used referance variables form the object data we get from 4.1
    
    5. Refactoring the code and converting it to function.
    
    6. Calling the function multiple times. So we have multiple AJAX call, which one arrives first will load first.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,41 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const getCountryData = function(country) {  // 5
+const request = new XMLHttpRequest();  // 1
+request.open(
+  'GET',
+  `https://countries-api-836d.onrender.com/countries/name/${country}`
+); // 2
+request.send(); // 3
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);  // 4.1
+  console.log(data);
+
+  const html = `
+    <article class="country">
+    <img class="country__img" src="${data.flag}" />  
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} M</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+    `; // 5.2
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+});
+
+};
+
+// 6
+getCountryData('bangladesh');
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('canada');
+
+
