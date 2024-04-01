2fd3202e252e222ba56c4a572a0cecfaadb115f6
Shafinul Pasha <jishan523@gmail.com>
2024-04-01 14:07:52 +0600

    Promises and the Fetch API
    There are actually some more options that we can specify here in the fetch function, if we'd like, but to make a simple get request like this one, all we really need is to pass in the URL and that's it.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -44,80 +44,97 @@ const countriesContainer = document.querySelector('.countries');
 
 
 /////////////////////////////////////////////////////////////
+// Callback Hell
 
-const renderCountry = function(data, className='') {  //1
-    const html = `
-    <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />  
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} M</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-    `; 
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-}
+// const renderCountry = function(data, className='') {  //1
+//     const html = `
+//     <article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />  
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} M</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//     `; 
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+// }
 
-const getCountryAndNeighbour = function(country) { 
+// const getCountryAndNeighbour = function(country) { 
 
-// AZAX Call 1    
-const request = new XMLHttpRequest();  
-request.open(
-  'GET',
-  `https://countries-api-836d.onrender.com/countries/name/${country}`
-); // 2
-request.send();
+// // AZAX Call 1    
+// const request = new XMLHttpRequest();  
+// request.open(
+//   'GET',
+//   `https://countries-api-836d.onrender.com/countries/name/${country}`
+// ); // 2
+// request.send();
 
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);  
-  console.log(data);
+// request.addEventListener('load', function () {
+//   const [data] = JSON.parse(this.responseText);  
+//   console.log(data);
 
-//   render country 1
-  renderCountry(data);
+// //   render country 1
+//   renderCountry(data);
 
-  const neighbour = data.borders[0];
-  //const [neihbour] = data.border  
+//   const neighbour = data.borders[0];
+//   //const [neihbour] = data.border  
 
-  if(!neighbour) return;
-// AZAX Call 2 // 2
+//   if(!neighbour) return;
+// // AZAX Call 2 // 2
 
- const request2 = new XMLHttpRequest();  
-request2.open(
-  'GET',
-  `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
-); // 2
-request2.send();
+//  const request2 = new XMLHttpRequest();  
+// request2.open(
+//   'GET',
+//   `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+// ); // 2
+// request2.send();
 
-request2.addEventListener('load', function(){
-    const data2 = JSON.parse(this.responseText);
-    console.log(data2);
+// request2.addEventListener('load', function(){
+//     const data2 = JSON.parse(this.responseText);
+//     console.log(data2);
 
-    renderCountry(data2, 'neighbour');
-})
+//     renderCountry(data2, 'neighbour');
+// })
   
-});
+// });
 
-// Get Neighbour country
+// // Get Neighbour country
 
-};
+// };
 
-// getCountryAndNeighbour('bangladesh');
-getCountryAndNeighbour('usa');
+// // getCountryAndNeighbour('bangladesh');
+// getCountryAndNeighbour('usa');
 
-setTimeout(() => {  // 3
-    console.log('1 second passed');
-    setTimeout(() => {
-      console.log('2 seconds passed');
-      setTimeout(() => {
-        console.log('3 second passed');
-        setTimeout(() => {
-          console.log('4 second passed');
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
+// setTimeout(() => {  // 3
+//     console.log('1 second passed');
+//     setTimeout(() => {
+//       console.log('2 seconds passed');
+//       setTimeout(() => {
+//         console.log('3 second passed');
+//         setTimeout(() => {
+//           console.log('4 second passed');
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+
+
+ /////////////////////////////////////////////////////////////
+// Promises and the Fetch API
+
+
+// // AZAX Call 1 (old way)   
+// const request = new XMLHttpRequest();  
+// request.open(
+//   'GET',
+//   `https://countries-api-836d.onrender.com/countries/name/${country}`
+// ); // 2
+// request.send();
+
+const request = fetch('https://countries-api-836d.onrender.com/countries/name/bangladesh')
+
