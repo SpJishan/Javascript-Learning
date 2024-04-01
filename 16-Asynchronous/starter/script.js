bbe7835adf299293248f71fd5a4dc8c89fab0c89
Shafinul Pasha <jishan523@gmail.com>
2024-04-01 12:43:01 +0600

    Welcome to Callback Hell
    What we need to implement is a sequence of AJAX call.
    
    1. Refactor HTML code to a function
    
    2. Making nested AZAX call
    
    3. We can another nested call back inside the nested call (like we do in setTImeout), beacuse many coutries mave 5/10 borders, but it is not a good practice because it will make our code messy and would make the code hard to understand.We can avoid it by using es6 promises.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,20 +5,49 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function(country) {  // 5
-const request = new XMLHttpRequest();  // 1
-request.open(
-  'GET',
-  `https://countries-api-836d.onrender.com/countries/name/${country}`
-); // 2
-request.send(); // 3
+// const getCountryData = function(country) { 
+// const request = new XMLHttpRequest();  
+// request.open(
+//   'GET',
+//   `https://countries-api-836d.onrender.com/countries/name/${country}`
+// ); // 2
+// request.send();
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);  // 4.1
-  console.log(data);
+// request.addEventListener('load', function () {
+//   const [data] = JSON.parse(this.responseText);  
+//   console.log(data);
 
-  const html = `
-    <article class="country">
+//   const html = `
+//     <article class="country">
+//     <img class="country__img" src="${data.flag}" />  
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} M</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//     `; 
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+// });
+
+// };
+
+
+// getCountryData('bangladesh');
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('canada');
+
+
+
+/////////////////////////////////////////////////////////////
+
+const renderCountry = function(data, className='') {  //1
+    const html = `
+    <article class="country ${className}">
     <img class="country__img" src="${data.flag}" />  
     <div class="country__data">
       <h3 class="country__name">${data.name}</h3>
@@ -28,17 +57,67 @@ request.addEventListener('load', function () {
       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
     </div>
   </article>
-    `; // 5.2
+    `; 
     countriesContainer.insertAdjacentHTML("beforeend", html);
     countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbour = function(country) { 
+
+// AZAX Call 1    
+const request = new XMLHttpRequest();  
+request.open(
+  'GET',
+  `https://countries-api-836d.onrender.com/countries/name/${country}`
+); // 2
+request.send();
+
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);  
+  console.log(data);
+
+//   render country 1
+  renderCountry(data);
+
+  const neighbour = data.borders[0];
+  //const [neihbour] = data.border  
+
+  if(!neighbour) return;
+// AZAX Call 2 // 2
+
+ const request2 = new XMLHttpRequest();  
+request2.open(
+  'GET',
+  `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+); // 2
+request2.send();
+
+request2.addEventListener('load', function(){
+    const data2 = JSON.parse(this.responseText);
+    console.log(data2);
+
+    renderCountry(data2, 'neighbour');
+})
+  
 });
+
+// Get Neighbour country
 
 };
 
-// 6
-getCountryData('bangladesh');
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('canada');
+// getCountryAndNeighbour('bangladesh');
+getCountryAndNeighbour('usa');
 
-
+setTimeout(() => {  // 3
+    console.log('1 second passed');
+    setTimeout(() => {
+      console.log('2 seconds passed');
+      setTimeout(() => {
+        console.log('3 second passed');
+        setTimeout(() => {
+          console.log('4 second passed');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
