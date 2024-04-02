b7fefc49354f03ed4969e662c07645202ea08d37
Shafinul Pasha <jishan523@gmail.com>
2024-04-02 15:40:41 +0600

    Handling Rejected Promises
     1. So this is a lot nicer just having one callback in the then. Instead we can handle all the errors no matter where they appear in the chain right at the end of the chain by adding a catch method.
    
     2. Besides then and catch there is also the finally method. It is not always useful but it is used to ake sure a property always use .

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,6 +3,31 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+    //1
+    const html = `
+      <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />  
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} M</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+      `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    // countriesContainer.style.opacity = 1;
+  };
+
+const renderError = function (msg) {
+    countriesContainer.insertAdjacentText('beforeend', msg);
+    // countriesContainer.style.opacity = 1;
+  };
+
 ///////////////////////////////////////
 
 // const getCountryData = function(country) {
@@ -135,25 +160,7 @@ const countriesContainer = document.querySelector('.countries');
 //////////////////////////////////////////////////////////////
 //Consuming Promises
 
-const renderCountry = function (data, className = '') {
-  //1
-  const html = `
-    <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />  
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} M</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-    </div>
-  </article>
-    `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
+
 // const getCountryData = function(country){
 //     fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`).then(function(response){  // 1
 //         console.log(response);
@@ -168,7 +175,7 @@ const renderCountry = function (data, className = '') {
 const getCountryData = function (country) {
   //Country 1
   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
-    .then(response => response.json())
+    .then(response => response.json())  
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders?.[0];
@@ -179,6 +186,18 @@ const getCountryData = function (country) {
       ); // not in here 1
     })
     .then(response => response.json()) // like this 1
-    .then(data => renderCountry(data, 'neighbour'));
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err =>{
+        console.error(`${err}`)
+        renderError(`Something went wrong : ${err.message}. Try again!`)
+    })            // handling errors
+    .finally(() => {
+        countriesContainer.style.opacity = 1;
+    })
+    ; 
 };
-getCountryData('usa');
+
+btn.addEventListener('click', function(){
+    
+    getCountryData('bangladesh');
+})
